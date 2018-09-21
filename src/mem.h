7c5211cf23136ecf29954350a22bc9f27451c43d
Clayton Bauman <claytonkb@gmail.com>
2018-09-21 13:54:27 -0700

    Moved mem.h, staging -> src

--- src/mem.h
+++ src/mem.h
@@ -25,7 +25,7 @@ void mem_sys_free(void *p);
 void mem_bank_alloc(alloc_bank *a, mword bank_size);
 void mem_bank_free(alloc_bank *a);
 
-void mem_new(pyr_cache *this_pyr, mword init_mem_size);
+void mem_new(babel_env *be, mword init_mem_size);
 void mem_destroy(mem_context *m);
 
 //void mem_reset_bank(alloc_bank *b);
@@ -40,30 +40,30 @@ mword mem_non_gc_insert(void *non_gc_ptr);
 
 void mem_non_gc_reset_pages(mword *page_ptr);
 
-mword *mem_alloc(pyr_cache *this_pyr, mword sfield);
-//mword *mem_alloc2(pyr_cache *this_pyr, mword alloc_sfield, access_size_sel access_size);
+mword *mem_alloc(babel_env *be, mword sfield);
+//mword *mem_alloc2(babel_env *be, mword alloc_sfield, access_size asize);
 
-void *mem_new_val(pyr_cache *this_pyr, mword size, mword init);
-void *mem_new_ptr(pyr_cache *this_pyr, mword size);
-void *mem_new_tptr(pyr_cache *this_pyr, const mword *hash, mword *bs);
-void *mem_new_cptr(pyr_cache *this_pyr, const mword *hash, mword *cptr);
+void *mem_new_val(babel_env *be, mword size, mword init);
+void *mem_new_ptr(babel_env *be, mword size);
+void *mem_new_tptr(babel_env *be, const mword *hash, mword *bs);
+void *mem_new_cptr(babel_env *be, const mword *hash, mword *cptr);
 
-void *_val(pyr_cache *this_pyr, mword value);
-void *_ptr(pyr_cache *this_pyr, mword *unsafe_ptr);
-//void *_tptr(pyr_cache *this_pyr, const mword *hash, mword *bs);
+void *_val(babel_env *be, mword value);
+void *_ptr(babel_env *be, mword *unsafe_ptr);
+//void *_tptr(babel_env *be, const mword *hash, mword *bs);
 
-mword *_newstr(pyr_cache *this_pyr, mword size8, char set_char);
-mword *_newbits(pyr_cache *this_pyr, mword size1);
+mword *_newstr(babel_env *be, mword size8, char set_char);
+mword *_newbits(babel_env *be, mword size1);
 
-mword *_cons(pyr_cache *this_pyr, mword *car, mword *cdr);
-mword *_dcons(pyr_cache *this_pyr, mword *car, mword *cdr, mword *cpr);
+mword *_cons(babel_env *be, mword *car, mword *cdr);
+mword *_dcons(babel_env *be, mword *car, mword *cdr, mword *cpr);
 
-void *_mkval(pyr_cache *this_pyr, mword array_size, ...);
-void *_mkptr(pyr_cache *this_pyr, mword array_size, ...);
-void *_mk_aop(pyr_cache *this_pyr, mword array_size, ...);
+void *_mkval(babel_env *be, mword array_size, ...);
+void *_mkptr(babel_env *be, mword array_size, ...);
+void *_mk_aop(babel_env *be, mword array_size, ...);
 
-mword *_mkls(pyr_cache *this_pyr, mword list_size, ...);
-mword *_mkdls(pyr_cache *this_pyr, mword list_size, ...);
+mword *_mkls(babel_env *be, mword list_size, ...);
+mword *_mkdls(babel_env *be, mword list_size, ...);
 
 // mem_alloc                -> vanilla memory-allocation (mword resolution)
 // mem_alloc_align_masked   -> vanilla memory-allocation (byte/bit resolution)
